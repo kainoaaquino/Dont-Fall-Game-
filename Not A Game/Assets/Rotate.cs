a669331d9fcaecccfcee81369a96e19cccd30164
Kainoa Aquino <kainoa104@gmail.com>
2020-10-30 22:39:41 -0700

    Patch

---
+++ Not A Game/Assets/Rotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public int rotateSpeed; 
+
+    void Start()
+    {
+        rotateSpeed = 2;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, rotateSpeed, 0, Space.World);
+    }
+}
---
+++ Not A Game/Assets/ScoringSystem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoringSystem : MonoBehaviour
+{
+    public GameObject scoreText;
+    public static int theScore;
+    public AudioSource collectSound;
+
+    void Update()
+    {
+        
+        scoreText.GetComponent<Text>().text = "SCORE: " + theScore;
+        
+    }
+}
